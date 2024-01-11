bf7f91779a9adcd1d6d1f89d4042c14a0ede0b89
okdef <owaingrandfield@gmail.com>
2024-01-11 00:28:12 -0600

    Main Method Created

---
+++ main.py
@@ -0,0 +1,19 @@
+#Main Method Roguelikeish Project by Okdef Started 1/11/2024
+#This project is a roguelike game built in python using libtcod python
+# Library Documentation: https://python-tcod.readthedocs.io/en/latest/
+
+from __future__ import annotations
+
+#Main 
+def main() -> None:
+    print("Hello World!")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
